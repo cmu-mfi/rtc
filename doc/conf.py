453e75192f14c2e7c198d1d953bcf61ddd0f06b6
Shobhit Aggarwal <shobhit.aggarwal97@gmail.com>
2024-09-21 18:32:35 -0400

    attempt 7

--- doc/conf.py
+++ doc/conf.py
@@ -27,7 +27,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'sphinx_rtd_theme'
 html_theme_options = {
-    'collapse_navigation': True,
+    'collapse_navigation': False,
     'sticky_navigation': True,
     'navigation_depth': 0,
 }
